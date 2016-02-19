e6f80cdcd0218bc338e4086925edd8f8240368d1
Jayin Ton <tonjayin@gmail.com>
2016-02-20 00:00:27 +0800

    add comment

--- libs/index.js
+++ libs/index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+/**
+ * 解析key-value头信息
+ * @param {Array} lines lines of the content
+ */
 function parser_setting (lines) {
   var setting = {}
   for (let i = 0;i < lines.length; i++) {
@@ -11,15 +15,26 @@ function parser_setting (lines) {
   }
   return setting
 }
-
+/**
+ * 解析description部分
+ * @param {Array} lines lines of the content
+ */
 function parser_description (lines) {
   return lines.join('\n').trim()
 }
-
+/**
+ * 解析答案
+ * @param {Array} lines lines of the content
+ */
 function parser_answer (lines) {
   return lines.join('\n').trim()
 }
-
+/**
+ * 获取字符串的
+ * @param {Array} arr 数组
+ * @param {int} start the start index since 0
+ * @param {int} end the end index 
+ */
 function subArray (arr, start, end) {
   let res = []
   for (let i = start; i <= end; i++) {
@@ -28,6 +43,10 @@ function subArray (arr, start, end) {
   return res
 }
 
+/**
+ * parser main()
+ * @param {string} content 
+ */
 function parser (content) {
   let lines = content.split('\n')
   let question = {
